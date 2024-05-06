5d651be2d6aef084b5a932a18365f58b7b6ba9b9
Chengzi-creator <1041379828@qq.com>
2024-05-06 15:43:39 +0800

    修改南瓜怪死后生成火的逻辑

--- Assets/GameMain/Scripts/Enemy/PumpkinFire.cs
+++ Assets/GameMain/Scripts/Enemy/PumpkinFire.cs
@@ -5,14 +5,36 @@ using GameFramework.Fsm;
 using Pathfinding;
 using UnityEngine;
 using UnityGameFramework.Runtime;
+using MyTimer;
 
 namespace GameMain
 {
     public class PumpkinFire : Enemy
-    {
+    {   
+        private CountdownTimer m_timer;
+
+        public void Init()
+        {
+            float Lasttime = 10f;
+            m_timer = new CountdownTimer();
+            m_timer.OnComplete += OnTimerComplete;
+            m_timer.OnTick += Timer;
+            m_timer.Initialize(Lasttime, true);
+        }
+        
         public override void GetBeaten(Vector2 force)
         {
             
+        }
+
+        public void Timer(float t)
+        {
+            //Log.Info(t);
+        }
+        public void OnTimerComplete()
+        {
+            RecycleSelf();
+            m_timer.Paused = true;
         }
     }
 }
